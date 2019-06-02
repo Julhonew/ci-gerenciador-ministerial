e1355e6005385a3106693019c5d0235451ffe150
Julho Justino <Julhonew@hgmail,com>
2019-06-02 12:29:11 -0300

    Primeiro commti

---
+++ application/views/listar.php
@@ -0,0 +1,63 @@
+﻿<?php 
+$this->load->view('top.php');
+
+// require_once('crud/select.php');
+// require_once('crud/delete.php');
+
+// $dados = new select();
+// $dados = $dados->table(false,'membros');
+
+// if(!empty($_GET) && isset($_GET)){
+// 	$delete = new delete($_GET['id'],'membros',$_GET['foto']);
+// }
+
+?>
+	
+	<div class="row col-md-12 ml-2">
+		<div class="col-md-2 "></div>
+		<div class="col-md-10 ">
+
+			<div class="titulo">
+				<ul>
+					<li><h1>Membros</h1></li>
+					<li class="float-right mt-2"><a href="cadastrar_membros.php"><input type="button" class="btn btn-info " value="+ Adicionar"></a></li>
+				</ul>
+			</div>
+				
+				
+
+			<table class="table"  border="1">
+					<tr>
+						<td class="td-center"><p>Selecionar</p></td>
+						<td class="td-center"><p>Nome da coluna</p></td>
+						<td class="td-center"><p>Nome da coluna</p></td>
+						<td class="td-center"><p>Nome da coluna</p></td>
+						<td class="td-center"><p>Nome da coluna</p></td>
+					</tr>	
+					<?php
+					// foreach($dados as $dado) {
+					?>
+						<tr>
+							<form action="credencial.php" method="POST">
+							<td class="td-center"><input type="checkbox" name="id" value=""></td>
+							<td class="td-center"><p><?php echo "valor"?> </p></td>
+							<td class="td-center"><p><?php echo "valor"?> </p></td>
+							<td class="td-center"><p><?php echo "valor"?> </p></td>
+							<td class="td-center"><p><?php echo "valor"?> </p></td>
+							<td class="td-center">
+								<a href="credencial.php"><input class="btn btn-info" type="button" value="Credencial"></a>
+								<a href="editar_membro.php"><input class="btn btn-info" type="button" value="Editar"></a>
+								<a href="membros.php"><input class="btn btn-danger"  type="button" value="X"></a>
+							</td>
+						</tr>	
+
+					<?php //} ?>
+			</table>	
+			<input type="submit" class="form-control btn btn-info" name="imprimir.php" value="imprimir">
+			</form>
+		</div>
+			
+	</div>
+
+
+<?php $this->load->view('bot.php')?>
---
+++ application/views/top.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IMECO - Missão Evangelica Casa De Oração</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\bootstrap-4.2.1\dist\css\bootstrap.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\css\estilo.css')?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\css\certificado.css')?>">
+	<script type="text/javascript" src="<?php echo base_url('assets\script\meu_script.js')?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets\nodes_modules\jquery\dist\jquery.js')?>"></script>
+
+</head>
+<body>
+	<div class="container-fluid zeraMargem" id="test">
+
+      <div class="fixed-top" id="barra_sup"></div>
+
+        <div class="sidebar">
+          <b><a class="active" href="index.php">IMECO</a></b>
+            <a href="membros.php">Membros</a>
+            <a href="escalas.php">Escalas</a>
+            <a href="cartas.php">Cartas</a>
+            <a href="certificados.php">Certificados</a> 
+            <a href="diretoria.php">Diretoria</a>
+            <a href="atualizacoes.php">Atualizações</a>
+        </div>
+
